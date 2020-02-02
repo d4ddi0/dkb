a03a3685f7c866b08f765679f088eb7f1e36363b
Joshua Clayton <joshua.clayton@3deolidar.com>
2020-02-01 17:12:34 -0700

    temp

--- main.c
+++ main.c
@@ -2,20 +2,25 @@
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
 
-ISR(INT0_vect)
+ISR(INT2_vect)
 {
-
+	//PORTC = (1 <<7); /* turn on the led */
 }
+
+#define FALLING_EDGE 2
+#define IRQ2_OFFSET(bits) (2 * bits)
 
 void setup_io()
 {
 	DDRC |=  (1 << 7); /* portc7 (LED AKA pin13) output */
+	EIMSK = 1 << IRQ2_OFFSET(1); /* enable IRQ2 */
+	EICRA = FALLING_EDGE << IRQ2_OFFSET(2); /* IRQ2 trigger type */
 }
 
 int main(void)
 {
 	setup_io();
-	PORTC = (1 <<7); /* turn on the led */
+	PORTC = (0 <<7); /* turn off the led */
 	sei();
 	set_sleep_mode(SLEEP_MODE_IDLE);
 	sleep_mode();
