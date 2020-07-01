e4b58c09139a2fc7277e04f13748553077d13604
Joshua Clayton <joshua.clayton@3deolidar.com>
2020-07-01 16:53:35 -0600

    recreate the blink sketch from arduino as a basic test

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
+#include <util/delay.h>
 
-ISR(INT2_vect)
-{
+//ISR(INT2_vect)
+//{
 	//PORTC = (1 <<7); /* turn on the led */
-}
+//}
 
 #define FALLING_EDGE 2
 #define IRQ2_OFFSET(bits) (2 * bits)
@@ -13,15 +14,21 @@ ISR(INT2_vect)
 void setup_io()
 {
 	DDRC |=  (1 << 7); /* portc7 (LED AKA pin13) output */
-	EIMSK = 1 << IRQ2_OFFSET(1); /* enable IRQ2 */
-	EICRA = FALLING_EDGE << IRQ2_OFFSET(2); /* IRQ2 trigger type */
+//	EIMSK = 1 << IRQ2_OFFSET(1); /* enable IRQ2 */
+//	EICRA = FALLING_EDGE << IRQ2_OFFSET(2); /* IRQ2 trigger type */
 }
 
 int main(void)
 {
 	setup_io();
-	PORTC = (0 <<7); /* turn off the led */
-	sei();
-	set_sleep_mode(SLEEP_MODE_IDLE);
-	sleep_mode();
+	PORTC = (1 <<7); /* turn off the led */
+//	sei();
+	//set_sleep_mode(SLEEP_MODE_IDLE);
+	//sleep_mode();
+	while (1) {
+	    _delay_ms(1000);
+	    PORTC = (1 <<7); /* turn on the led */
+	    _delay_ms(1000);
+	    PORTC = (0 <<7); /* turn off the led */
+	}
 }
